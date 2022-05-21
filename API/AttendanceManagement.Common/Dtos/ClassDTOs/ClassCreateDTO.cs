a5ed37622e8313a7fcea9f8e1498bee31bf1fb4c
pdattt <83204029+pdattt@users.noreply.github.com>
2022-05-21 20:50:37 +0700

    Adjust ClassDTOs Annotations

--- API/AttendanceManagement.Common/Dtos/ClassDTOs/ClassCreateDTO.cs
+++ API/AttendanceManagement.Common/Dtos/ClassDTOs/ClassCreateDTO.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +10,29 @@ namespace AttendanceManagement.Common.Dtos.ClassDTOs
 {
     public class ClassCreateDTO
     {
+        [Required]
         public string ClassName { get; set; }
 
-        public string? Location { get; set; }
+        [Required]
+        public string Location { get; set; }
 
-        public TimeSpan ClassStartTime { get; set; }
+        [Required]
+        public string ClassStartTime { get; set; }
 
-        public TimeSpan ClassEndTime { get; set; }
+        [Required]
+        public string ClassEndTime { get; set; }
 
+        [Required]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
         public DateTime ClassDateStart { get; set; }
 
+        [Required]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
         public DateTime ClassDateEnd { get; set; }
 
-        public string? DaysOfWeek { get; set; }
+        [Required]
+        public string DaysOfWeek { get; set; }
     }
 }
--- API/AttendanceManagement.Common/Dtos/ClassDTOs/ClassReadDTO.cs
+++ API/AttendanceManagement.Common/Dtos/ClassDTOs/ClassReadDTO.cs
@@ -12,16 +12,16 @@ namespace AttendanceManagement.Common.Dtos.ClassDTOs
 
         public string ClassName { get; set; }
 
-        public string? Location { get; set; }
+        public string Location { get; set; }
 
-        public DateTime ClassStartTime { get; set; }
+        public TimeSpan ClassStartTime { get; set; }
 
-        public DateTime ClassEndTime { get; set; }
+        public TimeSpan ClassEndTime { get; set; }
 
         public DateTime ClassDateStart { get; set; }
 
         public DateTime ClassDateEnd { get; set; }
 
-        public string? DaysOfWeek { get; set; }
+        public string DaysOfWeek { get; set; }
     }
 }
