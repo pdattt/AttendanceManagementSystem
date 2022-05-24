56a044eb076160ab6f7d32bc7857a031005712fd
pdattt <83204029+pdattt@users.noreply.github.com>
2022-05-24 19:00:51 +0700

    Add Session Model

---
+++ API/AttendanceManagement.Domain/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagement.Domain.Models
+{
+    public class Session
+    {
+        public int ClassId { get; set; }
+        public int EventId { get; set; }
+        public string Location { get; set; }
+    }
+}
