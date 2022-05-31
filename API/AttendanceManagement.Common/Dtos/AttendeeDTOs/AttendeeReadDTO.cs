ae1c9fd20236bad7f959fde1f4902df1233037ee
pdattt <83204029+pdattt@users.noreply.github.com>
2022-05-31 19:38:43 +0700

    Update lazy loading card

--- API/AttendanceManagement.Common/Dtos/AttendeeDTOs/AttendeeReadDTO.cs
+++ API/AttendanceManagement.Common/Dtos/AttendeeDTOs/AttendeeReadDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AttendanceManagement.Common.Dtos.CardDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ namespace AttendanceManagement.Common.Dtos.AttendeeDTOs
         public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string CardID { get; set; }
+        public List<CardReadDTO> Cards { get; set; }
         public string Role { get; set; }
     }
 }
