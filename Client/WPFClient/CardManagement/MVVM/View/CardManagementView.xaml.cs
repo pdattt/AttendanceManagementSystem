2f47c27b8473dc006653ca905340c7b376f5a92b
pdattt <83204029+pdattt@users.noreply.github.com>
2022-06-05 17:37:13 +0700

    Update Get data from serial port

--- Client/WPFClient/CardManagement/MVVM/View/CardManagementView.xaml.cs
+++ Client/WPFClient/CardManagement/MVVM/View/CardManagementView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CardManagement.MVVM.Model;
 using CardManagement.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Windows;
@@ -14,20 +15,46 @@ namespace CardManagement.MVVM.View
     {
         private SerialPort Port;
         private CardManagementViewModel CardVM;
-
         private List<Attendee> attendees;
-        //private readonly AttendeeBUS _bus;
 
         public CardManagementView()
         {
             InitializeComponent();
             CardVM = new CardManagementViewModel();
+
+            Port = new SerialPort();
+            Port.BaudRate = 9600;
+            Port.PortName = "COM4";
+            Port.Parity = Parity.None;
+            Port.DataBits = 8;
+            //Port.StopBits = StopBits.None;
+            Port.ReadTimeout = 500;
+            Port.DataReceived += Port_DataReceived;
+
+            //try
+            //{
+            //    Port.Open();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error");
+            //}
         }
 
         private async void CardManagement_Load(object sender, RoutedEventArgs e)
         {
             attendees = await CardVM.GetAllAttendee();
             gridAttendee.ItemsSource = attendees;
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(new EventHandler(displaydata_event));
+        }
+
+        private void displaydata_event(object sender, EventArgs e)
+        {
+            string in_data = Port.ReadLine();
         }
     }
 }
