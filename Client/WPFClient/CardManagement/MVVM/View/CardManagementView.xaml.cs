03f2757249faa417025bbd671097fd7dfb8798f3
pdattt <83204029+pdattt@users.noreply.github.com>
2022-06-07 21:05:25 +0700

    Update get data from serial port

--- Client/WPFClient/CardManagement/MVVM/View/CardManagementView.xaml.cs
+++ Client/WPFClient/CardManagement/MVVM/View/CardManagementView.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using CardManagement.MVVM.Model;
 using CardManagement.MVVM.ViewModel;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace CardManagement.MVVM.View
 {
@@ -16,29 +18,16 @@ namespace CardManagement.MVVM.View
         private SerialPort Port;
         private CardManagementViewModel CardVM;
         private List<Attendee> attendees;
+        private bool isPortOpen = false;
+        private ArrayList messages;
+        private int nRow;
 
         public CardManagementView()
         {
             InitializeComponent();
             CardVM = new CardManagementViewModel();
-
-            Port = new SerialPort();
-            Port.BaudRate = 9600;
-            Port.PortName = "COM4";
-            Port.Parity = Parity.None;
-            Port.DataBits = 8;
-            //Port.StopBits = StopBits.None;
-            Port.ReadTimeout = 500;
-            Port.DataReceived += Port_DataReceived;
-
-            //try
-            //{
-            //    Port.Open();
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message, "Error");
-            //}
+            messages = new ArrayList();
+            nRow = gridAttendee.SelectedIndex;
         }
 
         private async void CardManagement_Load(object sender, RoutedEventArgs e)
@@ -49,12 +38,75 @@ namespace CardManagement.MVVM.View
 
         private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            Application.Current.Dispatcher.Invoke(new EventHandler(displaydata_event));
+            SerialPort sp = (SerialPort)sender;
+
+            this.Dispatcher.Invoke(() =>
+            {
+                string indata = sp.ReadExisting();
+                //messages.Add(indata);
+
+                //var cardID = gridAttendee.Columns[4] as DataGridTextColumn;
+                //cardID.Binding = new Binding(indata);
+
+                int index = gridAttendee.Items.IndexOf(gridAttendee.SelectedItem);
+                ++index;
+
+                gridAttendee.Focus();
+                gridAttendee.SelectedItem = gridAttendee.Items[index];
+
+                //txt_MessageBox.Items.Add(indata.Replace('\r', ' ').Replace('\n', ' ').Trim());
+                //txt_Search.Text = indata;
+            });
         }
 
         private void displaydata_event(object sender, EventArgs e)
         {
-            string in_data = Port.ReadLine();
+            //string in_data = Port.ReadLine();
+            //txt_MessageBox.Items.Add(in_data + "\n");
+        }
+
+        private void btn_StartRead_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPortOpen)
+                return;
+
+            if (gridAttendee.SelectedItem == null)
+            {
+                gridAttendee.Focus();
+                gridAttendee.SelectedItem = gridAttendee.Items[0];
+            }
+            nRow = gridAttendee.SelectedIndex;
+            Port = new SerialPort();
+            Port.BaudRate = 9600;
+            Port.PortName = "COM4";
+            Port.Parity = Parity.None;
+            Port.DataBits = 8;
+            //Port.StopBits = StopBits.None;
+            Port.ReadTimeout = 500;
+            Port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+
+            try
+            {
+                Port.Open();
+                isPortOpen = true;
+                btn_StopRead.IsEnabled = true;
+                btn_StartRead.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void btn_StopRead_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isPortOpen)
+                return;
+
+            Port.Close();
+            isPortOpen = false;
+            btn_StopRead.IsEnabled = false;
+            btn_StartRead.IsEnabled = true;
         }
     }
 }
